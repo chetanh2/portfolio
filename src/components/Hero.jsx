20abdbf81f62f57dba743eb12666947232e69b72
chetanh2 <chetan@osiansoftware.com>
2025-07-03 11:20:43 +0530

    added new changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -40,7 +40,7 @@ const Hero = () => {
               Chetan Gudigar
             </motion.h2>
             <motion.span variants={childVariants} className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">
-              Front End Developer
+              Software Developer
             </motion.span>
             <motion.p variants={childVariants} className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
               {HERO_CONTENT}
