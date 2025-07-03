85f1f5d9d61a9cfbf190adf8342807d2c17de19c
chetanh2 <chetan@osiansoftware.com>
2025-07-03 10:38:31 +0530

    added animation using framer motion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,62 @@
 import React from 'react'
 import profilePic from "../assets/chetanNaukriDp.jpg"
 import { HERO_CONTENT } from '../constants';
+import {motion} from "framer-motion"
+
+const containerVariants = {
+  hidden:{opacity:0, x: -100},
+  visible:{
+    opacity:1,
+    x:0,
+    transition:{duration:0.5,staggerChildren:0.5}
+  }
+}
+const childVariants = {
+  hidden:{opacity:0, x: -100},
+  visible:{opacity:1, x:0, transition:{duration:0.5}}
+}
 const Hero = () => {
+
   return (
     <div className="pb-4 lg:mb-36">
       <div className="flex flex-wrap lg:flex-row-reverse">
         <div className="w-full lg:w-1/2">
           <div className="flex justify-center lg:p-8">
-            <img src={profilePic} alt="profile-pic" className='border border-stone-900 rounded-3xl' />
+            <motion.img
+              width={650}
+              height={650}
+              src={profilePic}
+              alt="profile-pic"
+              className="border border-stone-900 rounded-3xl"
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.5 }}
+            />
           </div>
         </div>
         <div className="w-full lg:w-1/2">
-          <div className="flex flex-col items-center lg:items-start mt-10">
-            <h2 className='pb-2 text-3xl lg:text-8xl tracking-tighter'>Chetan Gudigar</h2>
-            <span className='bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent'>Front End Developer</span>
-            <p className='my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter'>{HERO_CONTENT}</p>
-            <a href="/chetanResume.pdf" className='bg-white rounded-full p-4 text-sm text-stone-800 mb-10' target='blank' download> Download Resume</a>
-          </div>
+          <motion.div initial="hidden" animate="visible" variants={containerVariants} className="flex flex-col items-center lg:items-start mt-10">
+            <motion.h2 variants={childVariants} className="pb-2 text-3xl lg:text-8xl tracking-tighter">
+              Chetan Gudigar
+            </motion.h2>
+            <motion.span variants={childVariants} className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">
+              Front End Developer
+            </motion.span>
+            <motion.p variants={childVariants} className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
+              {HERO_CONTENT}
+            </motion.p>
+            <motion.a
+              variants={childVariants}
+              href="/chetanResume.pdf"
+              className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10"
+              target="blank"
+              download
+            >
+              {" "}
+              Download Resume
+            </motion.a>
+          </motion.div>
         </div>
-
       </div>
     </div>
   );
