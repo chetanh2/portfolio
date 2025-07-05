f7ec2a5c8647325cbeec060e52b42e42d9de4887
chetanh2 <chetan@osiansoftware.com>
2025-07-05 09:01:21 +0530

    added exp

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
         <motion.h2 whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -100 }} transition={{ duration: 0.5 }} className='my-10 text-center text-4xl'>Get in Touch</motion.h2>
         <div className="text-center tracking-tighter">
             <motion.p whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }} transition={{ duration: 1 }} className='my-4'>{CONTACT.address}</motion.p>
-            <motion.p whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }} transition={{ duration: 1 }} className='my-4'>{CONTACT.phoneNo}</motion.p>
+            <motion.a href={`tel:${CONTACT.phoneNo}`} whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }} transition={{ duration: 1 }} className='my-4 block'>{CONTACT.phoneNo}</motion.a>
             <a href="mailto:gudigarchetan@gmail.com" className='border-b'>{CONTACT.email}</a>
         </div>
     </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -5,72 +5,59 @@ import project4 from "../assets/projects/project-4.webp";
 
 export const HERO_CONTENT = `I am a passionate front end developer with a knack for crafting robust and scalable web applications. With 3.6 years of hands-on experience, I have honed my skills in front-end technologies like React,React Native and Next.js, as well as back-end technologies like Node.js, and Express. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile front end developer with a passion for creating efficient and user-friendly web applications. With 3.5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Ant design, Tailwind CSS, MUI and Node.js, . My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, reading books, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    year: "2022 - Present",
+    role: "Senior Frontend Developer",
+    company: "Osian infotech pvt ltd",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Created the company website from scratch, focusing on responsive design and optimized performance. Collaborated with UI/UX designers to ensure seamless user experiences. Integrated RESTful APIs. Implemented reusable components and maintained code quality through version control and code reviews. Successfully reduced page load time by 30% and improved overall application stability.`,
+
+    technologies: ["Javascript", "React.js", "Next.js", "TypeScript", "Tailwind Css", "Ant design","Bootstrap", "Node.js"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Youtube Clone",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "A YouTube clone web application featuring video playback, user authentication, and content management capabilities.",
+    technologies: ["HTML", "CSS", "React", "Javascript", "Tailwind"],
   },
   {
-    title: "Task Management App",
+    title: "Nps Calculator",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "An application for calculating the National Pension System (NPS).",
+    technologies: [
+      "HTML",
+      "CSS",
+      "React",
+      "Tailwind",
+      "Highcharts",
+      "Ant Design",
+    ],
   },
   {
     title: "Portfolio Website",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    technologies: ["HTML", "CSS", "React", "Tailwind CSS", "Framer Motion"],
   },
   {
-    title: "Blogging Platform",
+    title: "Swiggy Clone",
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "A web application clone of the popular food delivery service Swiggy, featuring a user-friendly interface, restaurant filtering, and menu item details.",
+    technologies: ["HTML", "CSS", "React", "Javascript", "Tailwind CSS"],
   },
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  // address: "767 Fifth Avenue, New York, NY 10153 ",
+  phoneNo: "+91 9082581804",
+  email: "gudigarchetan@gmail.com",
 };
